8fde02b7941ca9b4eb9d67d8958de3f2b0e42c92
pythonvava <dopunkupunk@gmail.com>
2021-02-07 00:09:18 +0900

    adding add installed Apps whitenoise

---
+++ staticfiles/js/timer.js
@@ -0,0 +1,6 @@
+const timeelement = document.getElementById('timer')
+const start = document.getElementById('start')
+const stop = document.getElementById('stop')
+const reset = document.getElementById('stop')
+
+
--- studypro/settings.py
+++ studypro/settings.py
@@ -73,6 +73,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'studyapp',
     'pic',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -158,11 +159,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static")
-)
+),
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
