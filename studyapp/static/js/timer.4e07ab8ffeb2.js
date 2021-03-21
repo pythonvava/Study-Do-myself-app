b44572102e1c1a264ec5a0202773c81bc0f96475
pythonvava <dopunkupunk@gmail.com>
2021-03-21 22:57:07 +0900

    Before modefy setplan

--- studyapp/static/js/timer.4e07ab8ffeb2.js
+++
@@ -1,6 +0,0 @@
-const timeelement = document.getElementById('timer')
-const start = document.getElementById('start')
-const stop = document.getElementById('stop')
-const reset = document.getElementById('stop')
-
-
